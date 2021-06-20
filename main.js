6b48080addfaea1947906a8837be32b34f17f7c3
John Bouakhasith <johnbeedot@gmail.com>
2021-06-20 17:46:32 -0400

    modified everything

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-const grid = document.querySelector("#grid");
+const container = document.querySelector(".container");
 
-function createCells(num) {
+function createGrid(num) {
+  pixelSize();
+  // create grid
   for (i = 0; i < num * num; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.className = "cell";
-    grid.appendChild(newDiv);
+    const createDiv = document.createElement("div");
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    container.appendChild(createDiv);
   }
-  grid.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "red";
-  });
+  const div = container.querySelectorAll("div");
+  div.forEach((pixel) => pixel.addEventListener("mouseover", pixelColor));
+}
+
+// reset pixel size
+function pixelSize() {
+  let pixels = container.querySelectorAll("div");
+  pixels.forEach((pixel) => pixel.remove());
+}
+
+// pixel color
+function pixelColor() {
+  this.style.backgroundColor = "red";
 }
