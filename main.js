38ba869c5379e539d0370cf8c35849089d984497
John Bouakhasith <johnbeedot@gmail.com>
2021-06-20 19:09:03 -0400

    modified everything

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const container = document.querySelector(".container");
+const lowButton = document.querySelector(".low-button");
+const medButton = document.querySelector(".medium-button");
+const highButton = document.querySelector(".high-button");
+const colorButtons = document.querySelectorAll(".color-option");
+const clearButton = document.querySelector(".clear-button");
+let color = "black";
 
+// default grid size on load
+createGrid(64);
+
+// create grid
 function createGrid(num) {
-  pixelSize();
-  // create grid
+  pixelReset();
   for (i = 0; i < num * num; i++) {
     const createDiv = document.createElement("div");
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
@@ -10,16 +19,38 @@ function createGrid(num) {
     container.appendChild(createDiv);
   }
   const div = container.querySelectorAll("div");
-  div.forEach((pixel) => pixel.addEventListener("mouseover", pixelColor));
+  div.forEach((pixel) => pixel.addEventListener("mouseover", colorPixel));
+}
+
+// color the pixel
+function colorPixel() {
+  if (color == "red") {
+    this.style.backgroundColor = "red";
+  } else if (color == "white") {
+    this.style.backgroundColor = "white";
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+// clicked color
+function colorOption(e) {
+  if (e.target.dataset.color == "red") {
+    color = "red";
+  } else if (e.target.dataset.color == "white") {
+    color = "white";
+  } else {
+    color = "black";
+  }
 }
 
 // reset pixel size
-function pixelSize() {
+function pixelReset() {
   let pixels = container.querySelectorAll("div");
   pixels.forEach((pixel) => pixel.remove());
 }
 
-// pixel color
-function pixelColor() {
-  this.style.backgroundColor = "red";
-}
+//  event listeners
+colorButtons.forEach((colorButton) =>
+  colorButton.addEventListener("click", colorOption)
+);
